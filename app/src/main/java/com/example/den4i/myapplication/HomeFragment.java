c6a0cc7062aa3d598f37099c45c84a0ca691734e
den4i <den4ig99@gmail.com>
2019-06-13 17:39:25 +0300

    w12

--- app/src/main/java/com/example/den4i/myapplication/HomeFragment.java
+++ app/src/main/java/com/example/den4i/myapplication/HomeFragment.java
@@ -23,7 +23,7 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home,container,false);
-        ListView listView = (ListView) view.findViewById(R.id.nav_home);
+        ListView listView = (ListView) view.findViewById(R.id.nav_home)
         return view;
     }
 
