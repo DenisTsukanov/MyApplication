24bc63efbd171abe19d8106e5435a349586acd00
den4i <den4ig99@gmail.com>
2019-06-19 17:15:23 +0300

    Revert "Added notifyDataSet"
    This reverts commit 2654f1ad

--- src/main/java/com/example/den4i/myapplication/HomeFragment.java
+++ src/main/java/com/example/den4i/myapplication/HomeFragment.java
@@ -7,11 +7,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 
 public class HomeFragment extends Fragment implements View.OnClickListener {
     EditText lcns;
--- src/main/java/com/example/den4i/myapplication/SortFragment.java
+++ src/main/java/com/example/den4i/myapplication/SortFragment.java
@@ -7,16 +7,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 public class SortFragment extends Fragment {
-    View view;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_sort,container,false);
-        TextView textView = view.findViewById(R.id.testText);
-        textView.setText("This is test string :)");
-        return view;
+        return inflater.inflate(R.layout.fragment_sort,container,false);
     }
 }
